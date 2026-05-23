d7dc85d1a3e35479023f41c98621e8f16551e07b
CurtisGino <curtis.gino@rincon.com>
2026-05-22 23:36:40 -0700

    feat: dynamic kickstart + templates

--- automation/render.py
+++ automation/render.py
@@ -3,9 +3,12 @@ import argparse
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 
-def render_template(j2_file: str, vars_file: str):
-    template_dir = Path("../templates")
-    out_file = Path(f"../.vault/{j2_file.rstrip(".j2")}")
+def render_template(j2_file: str, vars_file: str, file=None):
+    template_dir = Path("templates")
+    if file is not None:
+        out_file = Path(f".vault/{file}")
+    else:
+        out_file = Path(f".vault/{j2_file.rstrip(".j2")}")
     vars_file = Path(vars_file)
 
     with vars_file.open("r", encoding="utf-8") as f:
@@ -20,10 +23,11 @@ def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("--jinja-file", required=True, help="Jinja2 file to render.")
     parser.add_argument("--vars-file", required=True, help="Yaml file containing vars for rendering data.")
+    parser.add_argument("-o", "--output-file", help="Path of output file.")
     return parser
 
 if __name__ == "__main__":
     p = build_parser()
     args = p.parse_args()
 
-    render_template(args.jinja_file, args.vars_file)
+    render_template(args.jinja_file, args.vars_file, file=args.output_file)

eb5ad842bd56c1987cd5219d7ca640058fa11772
Gino Curtis <ginocurtis5@gmail.com>
2026-06-14 15:44:52 -0700

    feat: neovim setup

--- ux/vim/setup-vimrc.sh
+++ ux/vim/setup-vimrc.sh
@@ -3,3 +3,10 @@ cp ./vimrc "$HOME/.vimrc"
 
 echo "Vim environment setup."
 
+if [[ ! -d $HOME/.config/nvim ]]; then
+    mkdir -p $HOME/.config/nvim
+fi
+
+cp ./vimrc "$HOME/.config/nvim/init.vim
+echo "NeoVim environment also setup."
+

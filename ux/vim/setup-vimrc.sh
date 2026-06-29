df6c16b1c34b30ebee8d6bb52aa4e03d7e38fd1a
Gino Curtis <ginocurtis5@gmail.com>
2026-06-28 17:56:36 -0700

    fix: missing quote

--- ux/vim/setup-vimrc.sh
+++ ux/vim/setup-vimrc.sh
@@ -7,6 +7,6 @@ if [[ ! -d $HOME/.config/nvim ]]; then
     mkdir -p $HOME/.config/nvim
 fi
 
-cp ./vimrc "$HOME/.config/nvim/init.vim
+cp ./vimrc "$HOME/.config/nvim/init.vim"
 echo "NeoVim environment also setup."
 

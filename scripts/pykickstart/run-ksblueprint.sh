5ba83361bb965f219a7744baf1fdf08f697a4b0b
CurtisGino <curtis.gino@rincon.com>
2026-07-23 11:13:45 -0700

    feat: bash wrapper for ksblueprint.py

--- scripts/pykickstart/run-ksblueprint.sh
+++ scripts/pykickstart/run-ksblueprint.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/bash
+shopt -s nullglob
+
+configs=(vault/*.y*ml)
+if [ ${#configs[@]} -eq 0 ]; then
+    echo "no config files found in vault/" >&2
+    exit 1
+fi
+
+for yml in "${configs[@]}"; do
+    python3 ksblueprint.py -c "$yml" --pre-file diskpart-pre.bash.ks --embed-kickstart -o build/
+done
